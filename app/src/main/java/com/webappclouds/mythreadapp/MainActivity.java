8f0e259e73e0d58c63b1181c09f960d8bf430154
Dan Boehmke <Daniel@webappclouds.com>
2019-10-10 17:08:16 -0400

    Added delay

--- app/src/main/java/com/webappclouds/mythreadapp/MainActivity.java
+++ app/src/main/java/com/webappclouds/mythreadapp/MainActivity.java
@@ -1,16 +1,12 @@
 package com.webappclouds.mythreadapp;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,17 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+    }
+
+    public void ClickMyButton(View view){
+        long futureTime = System.currentTimeMillis() + 10000;
+        while(System.currentTimeMillis() < futureTime){
+            synchronized (this){
+                try{
+                    wait(futureTime-System.currentTimeMillis());
+                }catch (Exception e){}
             }
-        });
+        }
+        TextView myText = findViewById(R.id.myText);
+        myText.setText("Button Clicked");
     }
 
     @Override
